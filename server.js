c58e2b9029ddf816f7c43680b7a87e47ec8e635e
Maria Rosa Rivas <mariaro.rivas93@gmail.com>
2023-04-07 12:54:40 -0400

    api falsa

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
 
 class users {
     constructor() {
